221175b2f8834ef9c58841812abbbcad64d67056
Camillo-commits <58623609+Camillo-commits@users.noreply.github.com>
2021-03-31 18:03:04 +0200

    Finished algorithm for transportational problem

---
+++ src/modules/Odbiorca.java
@@ -0,0 +1,41 @@
+package com.company.modules;
+
+public class Odbiorca {
+    private double sprzedaż;
+    private int popyt;
+    private int actualPopyt;
+
+    public Odbiorca() {
+    }
+
+
+    public Odbiorca(double sprzedaż, int popyt) {
+        this.sprzedaż = sprzedaż;
+        this.popyt = popyt;
+        this.actualPopyt = popyt;
+    }
+
+    public double getSprzedaż() {
+        return sprzedaż;
+    }
+
+    public void setSprzedaż(double sprzedaż) {
+        this.sprzedaż = sprzedaż;
+    }
+
+    public int getPopyt() {
+        return popyt;
+    }
+
+    public void setPopyt(int popyt) {
+        this.popyt = popyt;
+    }
+
+    public int getActualPopyt() {
+        return actualPopyt;
+    }
+
+    public void setActualPopyt(int actualPopyt) {
+        this.actualPopyt = actualPopyt;
+    }
+}
